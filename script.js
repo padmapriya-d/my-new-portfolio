6a71583e7a47852b88e7f803592775ae6deca6b8
PADMAPRIYA D <902728@kgisledu.com>
2024-06-14 15:20:30 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+$(document).ready(function()
+{
+    $('#menu').click(function()
+    {
+        $(this).toggleclass('fa-times')
+        $('header').toggleclass('toggle')
+    });
+}
+);
+$(window).on('scroll load',function()
+{
+    $('#menu').removeClass('fa-times');
+    $('header').removeClass('toggle');
+}
+);
